9c8c685c0b9e2ab7f3c1c06e245660a0d0a8811a
David White <davidwhitedev@gmail.com>
2025-11-16 11:57:36 -0500

    chore: typing

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -3,13 +3,46 @@ import { Request, Response } from 'express';
 interface PageData {
     title: string;
     description: string;
-    screenshot: string; // Base64 encoded image string
+    screenshot?: string; // Base64 encoded image string
 }
 
-export const getPages = (req: Request, res: Response) => {
-    const pages: PageData[] = [
-        { title: "Example Title", "description": "Example Description", "screenshot": "b64encodedstring" }
-    ];
+interface GetPagesRequest {
+    urls: string[];
+    take_screenshots: boolean;
+}
 
-    res.json(pages);
+interface GetPagesResponse {
+    status: string;
+    data: PageData[];
+    validation: string[];
+}
+
+interface ErrorResponse {
+    status: string;
+    message: string;
+}
+
+export const getPages = (req: Request<{}, {}, GetPagesRequest>, res: Response<GetPagesResponse|ErrorResponse>) => {
+    try {
+        const pages: PageData[] = [
+            { title: "Example Title", "description": "Example Description", "screenshot": "b64encodedstring" }
+        ];
+
+        const pagesPayload: GetPagesResponse = {
+            status: "Success",
+            data: pages,
+            validation: [],
+        }
+
+        res.status(200).json(pagesPayload);
+    } catch (err) {
+        console.error(err);
+
+        const errorPayload: ErrorResponse = {
+            status: "Error",
+            message: "Failed to track pages"
+        }
+
+        res.status(500).json(errorPayload);
+    }
 }
